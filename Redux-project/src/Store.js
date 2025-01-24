c1865c571970b1d23b04166a67f9671351aacc57
invigilator27 <invigilator27@LRO-SH-20.vu.edu.pk>
2025-01-24 02:22:22 -0700

    new code added

---
+++ Redux-project/src/Store.js
@@ -0,0 +1,11 @@
+import {createStore,combineReducers} from 'redux';
+import customerReducer from "./features/customers/customerSlice";
+import accountReducer from "./features/accounts/accountSlice";
+
+const rootReducer = combineReducers({
+    customer :customerReducer, 
+    account :accountReducer
+})
+const store = createStore(rootReducer);
+
+export default store;
